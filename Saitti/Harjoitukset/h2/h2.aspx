f9c006abd1e9e482a47934ba927576c2cdf6498a
lahjo <h8306@student.jamk.fi>
2016-10-29 22:16:55 +0300

    Harjoitus 2 - valmis

---
+++ Saitti/Harjoitukset/h2/h2.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="h2.aspx.cs" Inherits="Harjoitukset_h2_h2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="dropMenuLotto" runat="server">
+            <asp:ListItem Text="SuomiLotto" Value="SuomiLotto"></asp:ListItem>
+            <asp:ListItem Text="VikingLotto" Value="VikingLotto"></asp:ListItem>
+        </asp:DropDownList>
+        <asp:TextBox ID="round" Text="" runat="server"></asp:TextBox>
+        <asp:Button ID="btnLotto" Text="Arvo" runat="server" OnClick="btnLotto_Click" />
+    </div>
+    <div>
+        <asp:ListBox id="Numbers" runat="server"></asp:ListBox>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/Harjoitukset/h2/h2.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Harjoitukset_h2_h2 : System.Web.UI.Page
+{
+	BLLotto lotto = new BLLotto();
+	
+    protected void Page_Load(object sender, EventArgs e)
+    {
+		
+    }
+	
+	protected void btnLotto_Click(object sender, EventArgs e)
+    {
+		Numbers.Items.Clear();
+		
+        try
+            {
+                int rounds = int.Parse(round.Text);
+
+                if (dropMenuLotto.SelectedItem.Text == "SuomiLotto") {
+                    for (int roundsLimit = 0; roundsLimit < rounds; roundsLimit++) {
+                        Numbers.Items.Add(lotto.SuomiLotto());
+                    }
+                } else if(dropMenuLotto.SelectedItem.Text == "VikingLotto") {
+                    for (int roundsLimit = 0; roundsLimit < rounds; roundsLimit++)
+                    {
+                        Numbers.Items.Add(lotto.VikingLotto());
+                    }
+                }
+            }
+		catch (Exception ex)
+			{
+			}
+    }
+}
