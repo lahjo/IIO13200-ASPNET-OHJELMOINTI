bcf3aad59e860037c98f897ab95c95b6fd90c5b0
lahjo <h8306@student.jamk.fi>
2016-11-05 19:46:01 +0200

    Tehtävä 3 valmis

---
+++ Saitti/Harjoitukset/h3/h3.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="h3.aspx.cs" Inherits="Harjoitukset_h3_h3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h3>Tänään on <%= DateTime.Now.ToString("dd-MM-yyyy") %></h3>
+        <h3>Valitsit päivän <asp:Label ID="selectedDate" Text="" runat="server"></asp:Label></h3>
+        <h3>Syntymäpäivääsi on <asp:Label ID="dayLeft" Text="" runat="server"></asp:Label> jäljellä</h3>
+         <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="calender_selection_changed"></asp:Calendar>     
+    </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/Harjoitukset/h3/h3.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Harjoitukset_h3_h3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void calender_selection_changed(object sender, EventArgs e) {
+        selectedDate.Text =  Calendar1.SelectedDate.ToString("dd-MM-yyyy");
+
+        TimeSpan i = new TimeSpan(0, 0, 0);
+
+        DateTime startDate = Convert.ToDateTime(DateTime.Today.ToString("dd-MM-yyyy").Trim());
+        DateTime endDate = Convert.ToDateTime(Calendar1.SelectedDate.ToString("dd-MM-yyyy").Trim());
+        i += endDate.Subtract(startDate);
+        int days = i.Days;
+
+        dayLeft.Text =  days.ToString();
+    }
+    
+}
